40310396c9ee87bf52094b2b7b29d7595a19b62c
Joe Brinkman <joe.brinkman@live.com>
2017-10-05 16:15:49 -0700

    Step 3 - Multi-docker example

--- demo.ps1
+++ demo.ps1
@@ -1,3 +1,4 @@
-write-host "Running the docker powershell example"
+write-host "Running the multi-docker example"
 
-docker run -it --rm microsoft/powershell:amazonlinux powershell
+docker run -it --rm -d --name amazon microsoft/powershell:amazonlinux powershell
+docker run -it --rm -d --name unknown microsoft/powershell powershell
