b045f4cdb93af9c564b60feed5e94428e3079bab
Joe Brinkman <joe.brinkman@live.com>
2017-10-06 10:18:35 -0700

    Step 7 - run iis with local content

--- demo.ps1
+++ demo.ps1
@@ -1,6 +1,6 @@
-write-host "Run IIS with port mapping"
+write-host "Add local content"
 
-docker run --rm -d -p 80:80 --name iis microsoft/iis:nanoserver
+docker run --rm -d -p 80:80 -v ${pwd}\website:c:\inetpub\wwwroot --name iis microsoft/iis:nanoserver
 
 $ip = docker inspect -f "{{ .NetworkSettings.Networks.nat.IPAddress }}" iis
 start-process "http://$ip"
